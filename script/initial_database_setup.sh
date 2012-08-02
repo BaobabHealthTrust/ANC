02bbfc9029134d40d27842c07d964c7b6c8d3c81
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2012-08-02 14:10:58 +0200

    Updated initialisation script

--- script/initial_database_setup.sh
+++ script/initial_database_setup.sh
@@ -22,6 +22,15 @@ set -x # turns on stacktrace mode which gives useful debug information
 #   cp config/database.yml.example config/database.yml
 # fi
 
+# Install gems
+
+sudo gem install devise -v="1.0.11" --include-dependencies
+sudo gem install warden -v="0.10.3" --include-dependencies
+sudo gem install devise -v="1.0.11" --include-dependencies
+sudo gem install composite_primary_keys -v="2.3.5.1" --include-dependencies
+sudo gem install json -v="1.6.1" --include-dependencies
+sudo gem install rest-client -v="1.6.7" --include-dependencies
+
 USERNAME=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['username']"`
 PASSWORD=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['password']"`
 DATABASE=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['database']"`
