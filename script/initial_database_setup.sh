a44ef47d65df78fab73e7f134c69254291c77ed4
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2012-08-02 14:55:53 +0200

    Updated initialisation script

--- script/initial_database_setup.sh
+++ script/initial_database_setup.sh
@@ -24,12 +24,14 @@ set -x # turns on stacktrace mode which gives useful debug information
 
 # Install gems
 
-sudo gem install devise -v="1.0.11" --include-dependencies --verbose
-sudo gem install warden -v="0.10.3" --include-dependencies --verbose
-sudo gem install devise -v="1.0.11" --include-dependencies --verbose
-sudo gem install composite_primary_keys -v="2.3.5.1" --include-dependencies --verbose
-sudo gem install json -v="1.6.1" --include-dependencies --verbose
-sudo gem install rest-client -v="1.6.7" --include-dependencies --verbose
+sudo gem install gems/*.gem --local --include-dependencies --verbose
+
+# sudo gem install devise -v="1.0.11" --include-dependencies --verbose
+# sudo gem install warden -v="0.10.3" --include-dependencies --verbose
+# sudo gem install devise -v="1.0.11" --include-dependencies --verbose
+# sudo gem install composite_primary_keys -v="2.3.5.1" --include-dependencies --verbose
+# sudo gem install json -v="1.6.1" --include-dependencies --verbose
+# sudo gem install rest-client -v="1.6.7" --include-dependencies --verbose
 
 USERNAME=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['username']"`
 PASSWORD=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['password']"`
