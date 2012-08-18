1909dada5a067ea4f3b6e4ab76b5d5cff0fdf876
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2012-08-18 10:21:19 +0200

    Fixed create_remote bug

--- lib/anc_service.rb
+++ lib/anc_service.rb
@@ -1536,7 +1536,7 @@ module ANCService
 
       begin
 
-        output = mechanize_browser.post("http://localhost:#{local_port}/patient/create_remote", demographics_params).body
+        output = mechanize_browser.post("http://#{demographic_server}:#{local_port}/patient/create_remote", demographics_params).body
 
       rescue Timeout::Error
         return 'timeout'
