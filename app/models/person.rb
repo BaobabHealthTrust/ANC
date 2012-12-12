d0155d4adbef2d6ff3a7430502dda8a8ec7eaa3e
phundi <kennethkapundi1@gmail.com>
2012-12-12 11:14:02 +0200

    fixed a bug when editing demographics, required methods were not available

--- app/models/person.rb
+++ app/models/person.rb
@@ -28,5 +28,56 @@ class Person < ActiveRecord::Base
     self.person_attributes.each{|row| row.void(reason) }
     # We are going to rely on patient => encounter => obs to void those
   end
+ def set_birthdate(year = nil, month = nil, day = nil)   
+    raise "No year passed for estimated birthdate" if year.nil?
+
+    # Handle months by name or number (split this out to a date method)    
+    month_i = (month || 0).to_i
+    month_i = Date::MONTHNAMES.index(month) if month_i == 0 || month_i.blank?
+    month_i = Date::ABBR_MONTHNAMES.index(month) if month_i == 0 || month_i.blank?
+    
+    if month_i == 0 || month == "Unknown"
+      self.birthdate = Date.new(year.to_i,7,1)
+      self.birthdate_estimated = 1
+    elsif day.blank? || day == "Unknown" || day == 0
+      self.birthdate = Date.new(year.to_i,month_i,15)
+      self.birthdate_estimated = 1
+    else
+      self.birthdate = Date.new(year.to_i,month_i,day.to_i)
+      self.birthdate_estimated = 0
+    end
+  end
+
+  def set_birthdate_by_age(age, today = Date.today)
+    self.birthdate = Date.new(today.year - age.to_i, 7, 1)
+    self.birthdate_estimated = 1
+  end
+  
+ def name
+    "#{self.names.first.given_name} #{self.names.first.family_name}" rescue nil
+  end  
+
+  def address
+    "#{self.addresses.first.city_village}" rescue nil
+  end 
+ def age(today = Date.today)
+    return nil if self.birthdate.nil?
+
+    # This code which better accounts for leap years
+    patient_age = (today.year - self.birthdate.year) + ((today.month - self.birthdate.month) + ((today.day - self.birthdate.day) < 0 ? -1 : 0) < 0 ? -1 : 0)
+
+    # If the birthdate was estimated this year, we round up the age, that way if
+    # it is March and the patient says they are 25, they stay 25 (not become 24)
+    birth_date=self.birthdate
+    estimate=self.birthdate_estimated
+    patient_age += (estimate && birth_date.month == 7 && birth_date.day == 1  && 
+        today.month < birth_date.month && self.date_created.year == today.year) ? 1 : 0
+  end
+
+  def age_in_months(today = Date.today)
+    years = (today.year - self.birthdate.year)
+    months = (today.month - self.birthdate.month)
+    (years * 12) + months
+  end
 
 end
