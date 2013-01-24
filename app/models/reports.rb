38f5c11116aeef189e39b24ff5fe0c4b515131cc
phundi <kennethkapundi1@gmail.com>
2013-01-24 15:50:13 +0200

    on art before visit one, pulled from bart2

--- app/models/reports.rb
+++ app/models/reports.rb
@@ -27,12 +27,13 @@ class Reports
         ConceptName.find_by_name("Reason For Visit").concept_id, 
         @startdate, @enddate]).collect{|e| e.patient_id}.uniq
     #raise @cohortpatients.to_yaml
-   	@bart_patients = on_art_in_bart
-    @bart_patients.to_yaml
-    @bart_patients.to_yaml
+   	@bart_patients = on_art_in_bart    
     @on_cpt = @bart_patients['on_cpt']
+    @on_art_before = @bart_patients['arv_before_visit_one']
+
  
     @bart_patients.delete("on_cpt")
+    @bart_patients.delete("arv_before_visit_one")
     @bart_patient_identifiers = @bart_patients.keys
   end
 
@@ -51,7 +52,7 @@ class Reports
       :select => ["patient_id, MAX(value_numeric) form_id"], 
       :conditions => ["concept_id = ? AND patient_id IN (?) AND encounter_datetime <= ? AND encounter_datetime >= ?",
         ConceptName.find_by_name("Reason for visit").concept_id, 
-        @cohortpatients, @enddate, @startdate]).collect{|e| e.patient_id}.uniq
+        @cohortpatients, @enddate, @startdate]).collect{|e| [e.patient_id, e.form_id]}.uniq
     
   end
   
@@ -111,11 +112,11 @@ class Reports
 
 	def week_of_first_visit_1
     #Encounter.find(:all, :joins => [:observations],
-      #:conditions => ["concept_id = ? AND value_text = ? AND (encounter_datetime >= ? " +
-        #  "AND encounter_datetime <= ?) AND patient_id IN (?)",
-        #ConceptName.find_by_name("WEEK OF FIRST VISIT").concept_id, "0-12",
-        #@startdate, @enddate, @cohortpatients]).collect{|e| e.patient_id}.uniq
-  Patient.find_by_sql("SELECT * FROM patient WHERE patient_id IN (SELECT person_id FROM obs LEFT OUTER JOIN encounter ON encounter.encounter_id = obs.encounter_id WHERE concept_id = (SELECT concept_id FROM concept_name WHERE name = 'WEEK OF FIRST VISIT') AND (value_coded IN (SELECT concept_id FROM concept_name WHERE name = '0-12') OR value_numeric = '0-12' OR value_boolean = '0-12' OR value_datetime = '0-12' OR value_text = '0-12') AND DATE_FORMAT(encounter_datetime, '%Y-%m-%d') >= '#{@start_date}' AND DATE_FORMAT(encounter_datetime, '%Y-%m-%d') <= '#{@end_date}')").collect{|p| p.patient_id}.uniq
+    #:conditions => ["concept_id = ? AND value_text = ? AND (encounter_datetime >= ? " +
+    #  "AND encounter_datetime <= ?) AND patient_id IN (?)",
+    #ConceptName.find_by_name("WEEK OF FIRST VISIT").concept_id, "0-12",
+    #@startdate, @enddate, @cohortpatients]).collect{|e| e.patient_id}.uniq
+    Patient.find_by_sql("SELECT * FROM patient WHERE patient_id IN (SELECT person_id FROM obs LEFT OUTER JOIN encounter ON encounter.encounter_id = obs.encounter_id WHERE concept_id = (SELECT concept_id FROM concept_name WHERE name = 'WEEK OF FIRST VISIT') AND (value_coded IN (SELECT concept_id FROM concept_name WHERE name = '0-12') OR value_numeric = '0-12' OR value_boolean = '0-12' OR value_datetime = '0-12' OR value_text = '0-12') AND DATE_FORMAT(encounter_datetime, '%Y-%m-%d') >= '#{@start_date}' AND DATE_FORMAT(encounter_datetime, '%Y-%m-%d') <= '#{@end_date}')").collect{|p| p.patient_id}.uniq
 	end
 
 
@@ -381,14 +382,16 @@ class Reports
 
 	def not_on_art   
     
-    # Encounter.find(:all, :joins => [:observations], :group => ["patient_id"],
-    #   :select => ["patient_id, MAX(encounter_datetime) encounter_datetime"],
-    #  :conditions => ["concept_id = ? AND (value_coded = ? OR value_text = ?) AND (encounter_datetime >= ? " +
-    #     "AND encounter_datetime <= ?) AND encounter.patient_id IN (?)",
-    #   ConceptName.find_by_name("HIV status").concept_id,
-    #  ConceptName.find_by_name("Positive").concept_id, "Positive",
-    #  @startdate, @end_date, @cohortpatients]).collect{|e| e.patient_id}
-    no_art = @cohortpatients - on_art_before
+   local =  Encounter.find(:all, :joins => [:observations], :group => ["patient_id"],
+       :select => ["patient_id, MAX(encounter_datetime) encounter_datetime"],
+      :conditions => ["concept_id = ? AND (value_coded = ? OR value_text = ?) AND (encounter_datetime >= ? " +
+         "AND encounter_datetime <= ?) AND encounter.patient_id IN (?)",
+       ConceptName.find_by_name("HIV status").concept_id,
+      ConceptName.find_by_name("Positive").concept_id, "Positive",
+      @startdate, @end_date, @cohortpatients]).collect{|e| e.patient_id}
+  
+    no_art = local - on_art_before
+    no_art = no_art.uniq
     no_art = [] if no_art.to_s.blank?
     return no_art
   end
@@ -408,13 +411,13 @@ class Reports
 
 	def on_art_zero_to_27
     
-		#local =	Encounter.find(:all, :joins => [:observations], :group => ["patient_id"], 
-    # :select => ["patient_id, MAX(encounter_datetime) encounter_datetime"],
-    # :conditions => ["concept_id = ? AND value_text = ? AND (encounter_datetime >= ? " +
-    #   "AND encounter_datetime <= ?) AND encounter.patient_id IN (?)",
-    # ConceptName.find_by_name("Date Antiretrovirals Started").concept_id,
-    # "At 0-27 weeks of Pregnancy",
-    #@startdate, @end_date, @cohortpatients]).collect{|e| e.patient_id} rescue []
+		local =	Encounter.find(:all, :joins => [:observations], :group => ["patient_id"], 
+     :select => ["patient_id, MAX(encounter_datetime) encounter_datetime"],
+     :conditions => ["concept_id = ? AND value_text = ? AND (encounter_datetime >= ? " +
+       "AND encounter_datetime <= ?) AND encounter.patient_id IN (?)",
+     ConceptName.find_by_name("Date Antiretrovirals Started").concept_id,
+     "At 0-27 weeks of Pregnancy",
+    @startdate, @end_date, @cohortpatients]).collect{|e| e.patient_id} rescue []
         
     @art_ids = "'" + @bart_patient_identifiers.join("','") + "'"
     
@@ -428,6 +431,7 @@ class Reports
         ob.person_id if (check <= (27*7).days && check >= 0.days)
       end
     } rescue []
+    remote = remote.concat(local).uniq
     remote = [] if remote.to_s.blank?
 		return remote
     
@@ -435,13 +439,13 @@ class Reports
 
 	def on_art_28_plus
     
-		#local  = Encounter.find(:all, :joins => [:observations], :group => ["patient_id"], 
-    #:select => ["patient_id, MAX(encounter_datetime) encounter_datetime"],
-    #:conditions => ["concept_id = ? AND value_text = ? AND (encounter_datetime >= ? " +
-    #   "AND encounter_datetime <= ?) AND encounter.patient_id IN (?)",
-    # ConceptName.find_by_name("Date Antiretrovirals Started").concept_id,
-    #"At 28+ of Pregnancy",
-    #@startdate, @end_date, @cohortpatients]).collect{|e| e.patient_id} rescue []
+		local  = Encounter.find(:all, :joins => [:observations], :group => ["patient_id"], 
+    :select => ["patient_id, MAX(encounter_datetime) encounter_datetime"],
+    :conditions => ["concept_id = ? AND value_text = ? AND (encounter_datetime >= ? " +
+       "AND encounter_datetime <= ?) AND encounter.patient_id IN (?)",
+     ConceptName.find_by_name("Date Antiretrovirals Started").concept_id,
+    "At 28+ of Pregnancy",
+    @startdate, @end_date, @cohortpatients]).collect{|e| e.patient_id} rescue []
 
     @art_ids = "'" + @bart_patient_identifiers.join("','") + "'"
 		remote =   Observation.find_by_sql("SELECT p.identifier, o.value_datetime, o.person_id FROM obs o 
@@ -453,7 +457,8 @@ class Reports
         ob.person_id if (@bart_patients["#{ident}"].to_date - (ob.value_datetime.to_date + 7.days)).days > (27*7).days
 			end
 		} rescue []
-   remote = [] if remote.to_s.blank?
+    remote = remote.concat(local).uniq
+    remote = [] if remote.to_s.blank?
     return remote
 	end
 
@@ -538,19 +543,33 @@ class Reports
     national_id = PatientIdentifierType.find_by_name("National id").id
 		patient_ids = PatientIdentifier.find(:all, :select => ['identifier, identifier_type'], :conditions => ["identifier_type = ? AND patient_id IN (?)", 		national_id, @cohortpatients]).collect{|ident|
       ident.identifier}.join(",")
-		paramz = Hash.new
-        
+    id_visit_map = []
+    patient_ids.split(",").each do |id|
+      next if id.nil?
+      patient_id = PatientIdentifier.find_by_identifier(id).patient_id      
+      if patient_id
+        date = Observation.find_by_sql("SELECT * FROM obs where person_id = #{patient_id} AND concept_id = (SELECT concept_id FROM concept_name where name = 'TYPE OF VISIT' AND value_numeric = 1 order by date_created LIMIT 1)").first.date_created.strftime("%Y-%m-%d") rescue nil
+        #:first, :conditions => ["person_id = ? AND concept_id =?",
+        #  patient_id, ConceptName.find_by_name("TYPE OF VISIT").id ]).date_created rescue nil
+       
+        value = "" + id + "|" + date   if !date.nil?
+        id_visit_map << value if !date.nil?
+      end
+
+    end
+		paramz = Hash.new   
 		paramz["ids"] = patient_ids
 		paramz["start_date"] = @startdate
 		paramz["end_date"] = @enddate
-         
-		server = CoreService.get_global_property_value("remote_servers.parent")
+    paramz["id_visit_map"] = id_visit_map.join(",")
+
+    server = CoreService.get_global_property_value("remote_servers.parent")
 
 		login = CoreService.get_global_property_value("remote_bart.username").split(/,/) rescue ""
 		password = CoreService.get_global_property_value("remote_bart.password").split(/,/) rescue ""
 				
 		uri = "http://#{login}:#{password}@#{server}/encounters/export_on_art_patients"	
 		patient_identifiers = JSON.parse(RestClient.post(uri, paramz)) rescue {}
-		return patient_identifiers
+    return patient_identifiers
   end  
 end
